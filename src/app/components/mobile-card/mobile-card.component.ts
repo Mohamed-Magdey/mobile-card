a62c7c732580e27e5606e3edee9b6a90e60ceb61
Mohamed-Magdey <ragabmohame6@gmail.com>
2024-09-19 23:56:07 +0300

    Complete card-header

--- src/app/components/mobile-card/mobile-card.component.ts
+++ src/app/components/mobile-card/mobile-card.component.ts
@@ -1,15 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
   selector: 'app-mobile-card',
   templateUrl: './mobile-card.component.html',
-  styleUrls: ['./mobile-card.component.scss']
+  styleUrls: ['./mobile-card.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class MobileCardComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class MobileCardComponent {
+  todayDate = new Date();
 }
